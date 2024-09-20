c8e625350f695c4f9acbc24aba778d7ce5fdf2c8
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-20 19:40:50 +0530

    setup project

---
+++ ai-course-generator/app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      Home Page
+      <Button>Subscribe</Button>
+    </div>
+  );
+}
