08f34263d6f1a65a508639c65458f62ef96782cd
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-22 22:32:31 +0530

    add Link to AddCourse Section

--- ai-course-generator/app/dashboard/_components/AddCourse.jsx
+++ ai-course-generator/app/dashboard/_components/AddCourse.jsx
@@ -2,6 +2,7 @@
 import { Button } from '@/components/ui/button';
 import { useUser } from '@clerk/nextjs'
 import React from 'react'
+import Link from 'next/link';
 
 function AddCourse() {
     const {user}=useUser();
@@ -12,7 +13,9 @@ function AddCourse() {
             <span className='font-bold'>{user?.fullName}</span></h2>
             <p className='text-sm text-gray-500'>Create new course with AI, Share qith friends and Earn from it</p>
        </div>
-       <Button>+ Create AI Course</Button>
+       <Link href={'/create-course'}>
+            <Button>+ Create AI Course</Button>
+       </Link>
     </div>
   )
 }
