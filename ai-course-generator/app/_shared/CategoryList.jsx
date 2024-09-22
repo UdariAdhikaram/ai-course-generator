fce1959a76b5b1687d82de8544131f9980cf7fd1
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-22 22:28:09 +0530

    set category list

---
+++ ai-course-generator/app/_shared/CategoryList.jsx
@@ -0,0 +1,20 @@
+export default [
+    {
+        id:1,
+        name:'Programming',
+        icon:'/coding.webp',
+        prompt:''
+    },
+    {
+        id:2,
+        name:'Health',
+        icon:'/lotus5.png',
+        prompt:''
+    },
+    {
+        id:3,
+        name:'Creative',
+        icon:'/creative.png',
+        prompt:''
+    },
+]
