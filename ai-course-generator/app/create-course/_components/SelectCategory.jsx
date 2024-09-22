1c7f4f793f5ff3913f64655481084aa49cb2d916
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-23 00:10:34 +0530

    change style

--- ai-course-generator/app/create-course/_components/SelectCategory.jsx
+++ ai-course-generator/app/create-course/_components/SelectCategory.jsx
@@ -4,7 +4,10 @@ import Image from 'next/image'
 
 const SelectCategory = () => {
   return (
-    <div className='grid grid-cols-3 gap-10 px-10 md:px-20'>
+    <div className='px-10 md:px-20'>
+      <h2 className='my-5'>Select the Course Category</h2>
+    
+    <div className='grid grid-cols-3 gap-10'>
         {CategoryList.map((item,index)=>(
             <div className='flex flex-col p-5 border 
             items-center rounded-xl hover:border-primary hover:bg-blue-50 cursor-pointer'>
@@ -12,6 +15,7 @@ const SelectCategory = () => {
                 <h2>{item.name}</h2>
             </div>    
         ))}
+    </div>
     </div>
   )
 }
