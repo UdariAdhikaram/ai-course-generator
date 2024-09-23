f9d317af75168e53410b31e45294c800e001896d
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-23 19:40:51 +0530

    create user input form

--- ai-course-generator/app/create-course/_components/TopicDescription.jsx
+++ ai-course-generator/app/create-course/_components/TopicDescription.jsx
@@ -1,18 +1,33 @@
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserInputContext } from '@/app/_context/UserinputContext'
 
 function TopicDescription() {
+
+const {userCourseInput,setUserCourseInput}=useContext(UserInputContext);
+ 
+  const handleInputChange=(fieldName,value)=>{
+    setUserCourseInput(prev=>({
+      ...prev,
+      [fieldName]:value
+    }))
+  }
   return (
     <div className='mx-20 lg:mx-44'>
         {/* Input Topic */}
-        <div className='mt-5'>
+        <div className='mt-20'>
             <label>💡 Write the topic for whic you want to generate a course (e.g., Python Course, Yoga, etc.): </label>
-            <Input placeholder={'Topic'} />
+            <Input placeholder={'Topic'} 
+            className="h-14 text-xl"
+            defaultValue={userCourseInput?.topic}
+            onChange={(e)=>handleInputChange('topic',e.target.value)}/>
         </div>
-        <div className='mt-5'>
+        <div className='mt-14'>
             <label>📝 Tell us more about your course, what you want to include in the course (Optional)</label>
-            <Textarea placeholder="About your Course" />
+            <Textarea placeholder="About your Course" className="h-24 text-xl"
+            defaultValue={userCourseInput?.description}
+            onChange={(e)=>handleInputChange('description',e.target.value)}/>
         </div>
         {/* Text Area Desc */}
     </div>
