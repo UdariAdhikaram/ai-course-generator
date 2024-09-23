9699576f1a852fb3e81141469ad5351b7e6eeefa
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-23 19:41:02 +0530

    create user input form

--- ai-course-generator/app/create-course/layout.jsx
+++ ai-course-generator/app/create-course/layout.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Header from '../dashboard/_components/Header'
+import { UserInputContext } from '../_context/UserinputContext'
 
 function CreateCourseLayout({children}) {
+
+const [userCourseInput,setUserCourseInput]=useState([]);
   return (
     <div>
+      <UserInputContext.Provider value={{userCourseInput,setUserCourseInput}}>
+        <>
         <Header/>
         {children}
+        </>
+        </UserInputContext.Provider>
+        
     </div>
   )
 }
